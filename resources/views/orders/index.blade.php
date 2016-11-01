3c0b66a0d4cfc736b3d9be6ba3bc5e892cc4b3f3
Abel Chang <ccabel42@gmail.com>
2016-11-02 00:31:37 +0800

    disable today new order button

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -131,10 +131,12 @@ $nextDate = Carbon::createFromDate($thisYear,$thisMonth,'1')->addMonth();
                             <span class="badge">{{$order->orderRoom->name}}</span>
                             @endif
                             @endforeach
+
+                            @if($thisDay != Carbon::now())
                             <a class="btn btn-xs btn-danger pull-right" href="{{ route('orders.create',['thisYear'=>$thisDay->year ,'thisMonth'=>$thisDay->month ,'thisDay'=>$thisDay->day]) }}" style="margin-left: 20px; color: white;">
-                                <i class="glyphicon glyphicon-plus"></i>
-                                        
+                                <i class="glyphicon glyphicon-plus"></i>                      
                             </a>
+                            @endif
  
                         
                         </a>
