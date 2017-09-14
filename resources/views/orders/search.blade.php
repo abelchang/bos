fd8e756001e6736183179790808999371936019c
Abel Chang <ccabel42@gmail.com>
2017-09-14 19:38:04 +0800

    add

--- resources/views/orders/search.blade.php
+++ resources/views/orders/search.blade.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 		<!-- Default panel contents -->
 		<div class="panel-heading">search</div>
 		<!-- Table -->
-		<table class="table table-hover table-sm table-responsive">
+		<table class="table table-responsive">
 			<thead>
 				<tr>
 					<th>#</th>
@@ -23,31 +23,32 @@ use Carbon\Carbon;
 					<!-- <th>地址</th> -->
 				</tr>
 			</thead>
+			
 			<tbody>
 				@foreach ($orders as $key=>$order)
 				<tr>
-					
-						<th scope="row">{{$key+1}}</th>
-						<td><a data-toggle="collapse" href="#orderCollapse{{$order->id}}">{{$order->customer}}</a></td>
-						<td>{{Carbon::parse($order->checkin)->format('D m/d')}}</td>
-						<td>{{Carbon::parse($order->checkout)->diffInDays(Carbon::parse($order->checkin))}}</td>
-						<td>{{$order->place->name}}</td>
-						<!-- <td>{{$order->mome}}</td> -->
-						<!-- <td>{{$order->phone}}</td> -->
-						<!-- <td>{{$order->placeOfBirth}}</td> -->
-						<!-- <td>{{$order->address}}</td> -->
-					
+					<th scope="row">{{$key+1}}</th>
+					<td>
+						<a data-toggle="collapse" href="#orderCollapse{{$order->id}}">{{$order->customer}}</a>
+						<div id="orderCollapse{{$order->id}}" class="panel-collapse collapse">
+							<ul class="list-group">
+								<li class="list-group-item">phone:<a href="tel:{{ $order->phone }}">{{ $order->phone }}</a></li>
+								<li class="list-group-item">MOME:{{$order->mome}}</li>
+								<li class="list-group-item">price:{{$order->price}}</li>
+								<li class="list-group-item">BD:{{$order->birthday}}</li>
+								<li class="list-group-item">BDP:{{$order->placeOfBirth}}</li>
+								<li class="list-group-item">ADR:{{$order->address}}</li>
+							</ul>
+						</div>
+					</td>
+					<td>{{Carbon::parse($order->checkin)->format('m/d')}}</td>
+					<td>{{Carbon::parse($order->checkout)->diffInDays(Carbon::parse($order->checkin))}}</td>
+					<td>{{$order->place->name}}</td>
+					<!-- <td>{{$order->mome}}</td> -->
+					<!-- <td>{{$order->phone}}</td> -->
+					<!-- <td>{{$order->placeOfBirth}}</td> -->
+					<!-- <td>{{$order->address}}</td> -->
 				</tr>
-				<div id="orderCollapse{{$order->id}}" class="panel-collapse collapse">
-					<ul class="list-group">
-						<li class="list-group-item">phone:<a href="tel:{{ $order->phone }}">{{ $order->phone }}</a></li>
-						<li class="list-group-item">MOME:{{$order->mome}}</li>
-						<!-- <li class="list-group-item">price:{{$order->price}}</li> -->
-						<li class="list-group-item">BD:{{$order->birthday}}</li>
-						<li class="list-group-item">BDP:{{$order->placeOfBirth}}</li>
-						<li class="list-group-item">ADR:{{$order->address}}</li>
-					</ul>
-				</div>
 				@endforeach
 			</tbody>
 		</table>
